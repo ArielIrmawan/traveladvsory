a517c8e25b55253ba7c24747b1f5ef7750db1668
ArielIrmawan <94830374+ArielIrmawan@users.noreply.github.com>
2023-05-31 19:30:16 +0700

    fixed

---
+++ src/components/List/List.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from "@material-ui/core";
+import useStyles from './styles';
+
+const List = () => {
+  return (
+    <div >
+      
+    </div>
+  )
+}
+
+export default List
